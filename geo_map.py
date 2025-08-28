7be6f8269139756c44742ea20bf52425c28c88b8
MIchaelLox <ghalimiaratrading@gmail.com>
2025-08-28 15:58:22 -0400

    Update geo_map.py

--- geo_map.py
+++ geo_map.py
@@ -1,1 +1,16 @@
+# ===========================================
+# File: geo_map.py
+# Purpose: Display restaurants on map
+# ===========================================
 
+
+class GeoMap:
+def __init__(self, geocoded_data):
+self.geocoded_data = geocoded_data
+
+
+def render_map(self):
+"""
+Plot restaurants on map with color-coded risk categories.
+"""
+pass
